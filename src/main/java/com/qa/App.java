0d3f50c1163bfb565b50ac83c1ba3ad36ae0dfc9
Adrian <adrianek90@hotmail.com>
2026-02-24 14:27:37 +0000

    EDIT: add another change in

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Test"));
+        System.out.println(sayHelloToSomeone("Tester"));
         System.out.println(sayGoodbye());
     }
 
